88a4ee1e0f9f5b96e8e35422f18c22b9bf2d8f5f
Viktor Bezai <32422942+TitanVA@users.noreply.github.com>
2021-12-01 20:48:03 +0300

    Add cities ...cities/add with CreateView

--- cities/urls.py
+++ cities/urls.py
@@ -6,4 +6,5 @@ from cities.views import *
 urlpatterns = [
     path('', home, name="home"),
     path('detail/<int:pk>/', CityDetailView.as_view(), name="detail"),
+    path('add/', CityCreateView.as_view(), name="create"),
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,29 +1,37 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import DetailView, CreateView
 
+from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
 
 __all__ = (
     "home",
     "CityDetailView",
+    "CityCreateView"
 )
 
 
-def home(request, pk=None):
-    # if pk:
-    #     # city = City.objects.filter(pk=pk).first()
-    #     # city = City.objects.get(pk=pk)
-    #     city = get_object_or_404(City, id=pk)
-    #     context = {"city": city,}
-    #     return render(request, "cities/detail_city.html", context=context)
-    # else:
+def home(request):
+    if request.method == "POST":
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
     cities = City.objects.all()
-    context = {"cities": cities,}
+    form = CityForm()
+    context = {"cities": cities, "form": form}
     return render(request, "cities/home.html", context=context)
 
 
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = "cities/detail_city.html"
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = "cities/create.html"
+    success_url = reverse_lazy("cities:home")
 
